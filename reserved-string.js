8d7ac2ec53e2ba705740fc8cd76accdba1d44931
maksicode <maksim-mih23@yandex.ru>
2024-08-16 15:31:05 +0300

    zadacha1

---
+++ reserved-string.js
@@ -0,0 +1,19 @@
+let character = 'Hello';
+let count = 8;
+
+// Step 26
+// A method in JavaScript is a function that's associated with certain values or objects. 
+// An example you've already encountered is the .log() method, which is part of the console object.
+
+// Arrays have their own methods, and the first you will explore is the .push() method. 
+// This allows you to "push" a value to the end of an array. 
+// Here is an example to add the number 12 to the end of an array:
+
+// Example Code
+// array.push(12);
+// Use .push() to add the string "freeCodeCamp" to the end of your rows array. 
+// Add this code before your console.log so you can see the change you made to your array.
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
