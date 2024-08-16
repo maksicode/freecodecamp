a346f898ed34337e4f330ebc4c5c7e6cdc9a409a
maksicode <maksim-mih23@yandex.ru>
2024-08-16 15:53:27 +0300

    2

---
+++ reserved-string-2.js
@@ -0,0 +1,17 @@
+let character = 'Hello';
+let count= 8;
+
+// Step 27
+// Another method essential for this project is the .pop() method. 
+// It removes the last element from an array and returns that element.
+
+// When a method returns a value, you can think of it as giving the value back to you,
+//  making it available for use in other parts of your code.
+
+// Create a new variable called popped and assign it the result of rows.pop(). 
+// Then, log popped to the console.
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(popped);
