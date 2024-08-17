dc332a814ef03e9f8a89ae2de0e861fd462e1ea6
maksicode <maksim-mih23@yandex.ru>
2024-08-17 13:59:00 +0300

    Step 36

---
+++ step-36.js
@@ -0,0 +1,12 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+// Step 36
+// Your iteration statement will tell your loop what to do with the iterator after each run.
+
+// When you reassign a variable, you can use the variable to reference the previous value before the reassignment. This allows you to do things like add three to an existing number. For example, bees = bees + 3; would increase the value of bees by three.
+
+// Use that syntax to replace your "iteration" string with a reassignment statement that increases i by one.
+
+for (let i = 0; i < count; i = i + 1) {}
