76d51f42a7b2ba13ca88bfc358b79a0d30f02d69
maksicode <maksim-mih23@yandex.ru>
2024-09-03 22:35:31 +0300

    Step 2

--- step2.js
+++ step2.js
@@ -1,15 +1,13 @@
 function getAverage(scores) {
     let sum = 0;
-  
     for (const score of scores) {
       sum += score;
     }
-  
     return sum / scores.length;
   }
   
   function getGrade(score) {
-    if (score === 100) {
+    if (score >= 100) {
       return "A++";
     } else if (score >= 90) {
       return "A";
@@ -29,12 +27,10 @@ function getAverage(scores) {
   }
 
   function studentMsg(totalScores, studentScore) {
-    
-    
     if (studentScore > 59) {
-      return('Class average: ' + getAverage(totalScores) + '.' + ' Your grade: ' + getGrade() + '. You passed the course')
+      return('Class average: ' + getAverage(totalScores) + '.' + ' Your grade: ' + getGrade(studentScore) + '. You passed the course')
     } else {
-      return('Class average: ' + getAverage(totalScores) + '.' + ' Your grade: ' + getGrade() + '. You failed the course')
+      return('Class average: ' + getAverage(totalScores) + '.' + ' Your grade: ' + getGrade(studentScore) + '. You failed the course')
     }
    }
    console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
